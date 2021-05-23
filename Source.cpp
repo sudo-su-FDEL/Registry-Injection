33507c4e28bcbd328200b492277c1b8848374d3c
sudo-su-FDEL <58496330+sudo-su-FDEL@users.noreply.github.com>
2021-05-23 01:03:19 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,10 +6,12 @@ using namespace std;
 int main(){
 HKEY hkey;
 LONG regoOpenResult;
+//Directory given is a template, CHANGE AS NEEDED
 WCHAR PATH[] = L"C:\\Users\\NAME\\DIRECTORY\\NameOfFile";
-
+  
+//Path in the regisrty, this path is currently set in Start Up programs meaning the program will run or will open as soon as computer boots.
 regoOpenResult = RegOpenKeyEx(HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_ALL_ACCESS, &hkey);
-
+//"NAME IF VALUE" is the name that will appear in the registry
 RegSetValueEx(hkey, L"NAME OF VALUE", 0, REG_SZ, (LPBYTE)PATH, sizeof(PATH));
 
 RegCloseKey(hkey); 
