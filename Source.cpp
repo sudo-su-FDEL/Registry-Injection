c236465611c2174de48cc7ce9079ff3af34619cd
sudo-su-FDEL <58496330+sudo-su-FDEL@users.noreply.github.com>
2021-05-23 00:30:58 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
 HKEY hkey;
 LONG regoOpenResult;
-WCHAR PATH[] = L"C:\\Users\\NAME\\testdirect3\\NameOfFile";
+WCHAR PATH[] = L"C:\\Users\\NAME\\DIRECTORY\\NameOfFile";
 
 regoOpenResult = RegOpenKeyEx(HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_ALL_ACCESS, &hkey);
 
