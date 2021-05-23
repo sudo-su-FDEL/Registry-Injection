82ed96d1573f0499b723a41a9d7f884b908846ea
sudo-su-FDEL <58496330+sudo-su-FDEL@users.noreply.github.com>
2021-05-23 00:30:36 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<windows.h>
+#include <windows.h>
 
 using namespace std;
 
@@ -10,7 +10,7 @@ WCHAR PATH[] = L"C:\\Users\\NAME\\testdirect3\\NameOfFile";
 
 regoOpenResult = RegOpenKeyEx(HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_ALL_ACCESS, &hkey);
 
-RegSetValueEx(hkey, L"Windows Defenderss", 0, REG_SZ, (LPBYTE)PATH, sizeof(PATH));
+RegSetValueEx(hkey, L"NAME OF VALUE", 0, REG_SZ, (LPBYTE)PATH, sizeof(PATH));
 
 RegCloseKey(hkey); 
 
